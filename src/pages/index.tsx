c9af5ec648ef4aca419bd69208ec127a64426444
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-04-24 11:58:54 -0300

    now using axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { api } from "../services/api";
+
+type Episode = {
+    id: string;
+    title: string;
+    member: string;
+}
+
+type HomeProps = {
+    episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
     console.log(props.episodes)
 
     return (
@@ -10,10 +23,14 @@ export default function Home(props) {
 }
 
 
-export async function getStaticProps(){
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
-
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit:12,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
     return {
         props: {
             episodes: data,
